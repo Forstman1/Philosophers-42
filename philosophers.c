8c73540f371bd4a332c3f55a7201dd283fe61432
Sami Hafid <sahafid@e1r7p2.1337.ma>
2022-05-09 18:39:20 +0100

    test

--- philosophers.c
+++ philosophers.c
@@ -15,29 +15,74 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+
+typedef struct s_rules
+{
+	int	nb_philo;
+	int	time_to_die;
+	int	time_to_sleep;
+	int	time_to_eat;
+	int number_times_to_eat;
+
+}  t_rules;
+
+
+typedef struct s_philosopers 
+{
+	pthread_t philo;
+	struct s_philosophers *next;
+} t_philo;
 
 void*	philosophers_state()
 {
-	printf("Alive\n");
+	printf("philosopher Alive\n");
+	return 0;
+}
+
+
+void	entring_arguments(t_rules *rules, char	**argv)
+{
+	rules->nb_philo = atoi(argv[1]);
+	rules->time_to_die = atoi(argv[2]);
+	rules->time_to_eat = atoi(argv[3]);
+	rules->time_to_sleep = atoi(argv[4]);
+	if (argv[5] != NULL)
+		rules->number_times_to_eat = atoi(argv[5]);
+}
+
+void	initing_philosophers(t_philo **philo)
+{
+	t_philo *lst;
+	lst = *philo;
+	
 }
 
 int main(int argc, char	*argv[])
 {
-	pthread_t t[3];
+	t_philo *philo;
+	t_rules rules;
+	int j;
 	int i;
 	
 	i = 0;
-	while (i < 3)
-	{
-		pthread_create(&t[i], NULL, &philosophers_state, NULL);
-		i++;
-	}
-	i = 0;
-	while (i < 3)
-	{
-		pthread_join(t[i], NULL);
-		i++;
-	}
 
+	if (argc != 5 && argc != 6)
+		return (write(1, "entre the correct amount of arguments", 38));
+	entring_arguments(&rules, argv);
+	initing_philosophers(&philo);
+
+	// while (i < 5)
+	// {
+	// 	pthread_create(&t[i], NULL, &philosophers_state, NULL);
+	// 	i++;
+	// }
+	i = 0;
+	// while (i < 5)
+	// {
+	// 	pthread_join(t[i], NULL);
+	// 	i++;
+	// }
 	return 0;
 }
